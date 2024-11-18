a3ebaecdcf31f2ecabc47875b96cc730746570e6
snjezaanna <86197534+snjezaanna@users.noreply.github.com>
2024-11-18 01:19:49 +0100

    Add files via upload

--- 2022/K1-03A/main.c
+++ 2022/K1-03A/main.c
@@ -3,40 +3,53 @@
 
     /*  Napisati program u programskom
 jeziku C (dozvoljeno je korištenje funkcija samo
-iz stdio.h) koji uèitava prirodne brojeve n i k
-(k<=10), a zatim ispisuje vrednost sledeæe
-sume:   sumFrom (i=1 to n) of sp(i^k)
-gde je sp(t) suma parnih cifara broja t. Na
-primer, sp(128)=2+8=10. */
+iz stdio.h) koji na poèetku uèitava prirodan broj
+n (n<=10), a zatim uèitava realan broj x i
+ispisuje vrednost e^x. Uèitavanje broja x i ispis
+vrednosti e^x treba ponavljati dok god je uèitano
+x>=1. Vrednost e^x može da se izraèuna
+korištenjem sledeæe formule:
+e^x=1+x/1!+x^2/2!+...+x^n/n!    */
+
+long long factorial(int n) {
+    long long result=1;
+    for(int i=1; i<=n; i++) {
+        result*=i;
+    }
+    return result;
+}
+
+double power(double baza, int eksp)  {
+    double rezultat=1;
+    for(int i=1; i<=eksp; i++) {
+        rezultat*=baza;
+    }
+    return rezultat;
+}
 
 int main()
 {
-    int n, k, sp=0;
+    int n; double x, ex;
     do {
-        printf("Unesi prirodne brojeve n i k takve da je k<=10! ");
-        scanf("%d %d", &n, &k);
+        printf("Unesi prirodan broj n<=10! ");
+        scanf("%d", &n);
     }
-    while(n<=0 || k<=0 || k>10);
+    while(n>10 || n<=0);
 
-
-    for(int i=1; i<=n; i++) {
-        int temp=i;
-        for(int j=2; j<=k; j++) {
-            temp=temp*i;
+    do{
+        printf("\nUnesi realan broj x! ");
+        scanf("%lf", &x);
+//        if(x<1) {
+//                break;
+//        }
+        ex=1.0;
+        for(int i=1; i<=n; i++) {
+        ex+=power(x, i)/factorial(i);
         }
-        int temp_sum=0;
+        printf("Vrijednost e^x je: %lf\n", ex);
+    } while(x>=1);
 
-        while(temp!=0)  {
-            if(temp%2==0)   {
-                temp_sum+=(temp%10);
-            }
-            temp/=10;
-        }
-        sp+=temp_sum;
-    }
-    printf("%d", sp);
-
-
+    printf("Kraj programa - uneseni je broj manji od 1\n");
 
     return 0;
 }
